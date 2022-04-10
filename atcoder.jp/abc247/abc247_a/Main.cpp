40636f4cc617325610bcf7be921865107e072155
yuukimasir0 <plusion00@icloud.com>
2022-04-10 12:07:01 +0000

    [AC] abc247 abc247_a

---
+++ atcoder.jp/abc247/abc247_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll inf = 80000000000;
+const ll prime1 = 1000000007;
+const ll prime2 = 998244353;
+const double pi = 3.141592653;
+
+string s;
+
+int main(){
+  cin >> s;
+  s.erase(3);
+  cout << "0" + s << endl;
+}
