dc0acbd8cd94f536d0d847422b0230e0fc0fe500
yuukimasir0 <plusion00@icloud.com>
2022-05-21 12:01:36 +0000

    [AC] abc252 abc252_a

---
+++ atcoder.jp/abc252/abc252_a/Main.cpp
@@ -0,0 +1,19 @@
+#pragma region header
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+#pragma endregion header
+
+ll n;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cin >> n;
+    char ans = n;
+    cout << ans << "\n";
+}
