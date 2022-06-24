7e93f21da25e493c62dfe24f6a533dcecaa43223
yuukimasir0 <plusion00@icloud.com>
2022-06-24 15:34:31 +0000

    [AC] typical90 typical90_aa

---
+++ atcoder.jp/typical90/typical90_aa/Main.cpp
@@ -0,0 +1,44 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef long long ll;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+
+struct in {
+  template <class T> operator T() {
+    T t;
+    std::cin >> t;
+    return t;
+  }
+};
+
+struct Solver
+{
+  void solve()
+  {
+    ll n = in();
+    set<string> s;
+    for (int i = 1; i <= n; i++) {
+      string tmp = in();
+      if(!s.count(tmp)) {
+        cout << i << "\n";
+        s.insert(tmp);
+      }
+    }
+  }
+};
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  Solver solver;
+  solver.solve();
+}
