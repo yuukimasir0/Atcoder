cc2db863b8d290b5b12e79690f638ebf59d4e029
yuukimasir0 <plusion00@icloud.com>
2022-07-02 18:41:56 +0000

    [AC] abc231 abc231_a

---
+++ atcoder.jp/abc231/abc231_a/Main.cpp
@@ -0,0 +1,49 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef long long ll;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+
+namespace cat
+{
+    inline ll mod(ll x, ll m)
+    {
+        return (((x % m) + m) % m);
+    }
+}
+
+struct in
+{
+    template <class T>
+    operator T()
+    {
+        T t;
+        std::cin >> t;
+        return t;
+    }
+};
+
+struct Solver
+{
+    void solve()
+    {
+        double d = in();
+        d /= 100;
+        printf("%.12f", d);
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    Solver solver;
+    solver.solve();
+}
