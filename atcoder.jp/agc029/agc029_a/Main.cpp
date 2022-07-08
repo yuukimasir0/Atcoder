f12efb04c1eeca73491f78bd638dfa690ea1ee5d
yuukimasir0 <plusion00@icloud.com>
2022-07-08 20:10:35 +0000

    [AC] agc029 agc029_a

---
+++ atcoder.jp/agc029/agc029_a/Main.cpp
@@ -0,0 +1,58 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef long long ll;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+
+namespace cat
+{
+    inline ll mod(ll x, ll m)
+    {
+        return (((x % m) + m) % m);
+    }
+}
+
+struct in
+{
+    template <class T>
+    operator T()
+    {
+        T t;
+        std::cin >> t;
+        return t;
+    }
+};
+
+struct Solver
+{
+    void solve()
+    {
+        string s = in();
+        ll n = s.length();
+        ll k = 0;
+        ll ans = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            if (s[i] == 'B')
+                k++;
+            else
+                ans += k;
+        }
+        cout << ans << "\n";
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    Solver solver;
+    solver.solve();
+}
