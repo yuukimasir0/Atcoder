957fc6c0f69d1074aa2e2f0e9877266ad862436a
yuukimasir0 <plusion00@icloud.com>
2022-07-16 21:09:49 +0000

    [AC] abc126 abc126_c

---
+++ atcoder.jp/abc126/abc126_c/Main.cpp
@@ -0,0 +1,79 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef intmax_t ll;
+typedef int_fast64_t i64;
+typedef int_fast32_t i32;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+
+template <typename T, typename U>
+istream &operator>>(istream &i, vc<pair<T, U>> &v)
+{
+    for (auto &&[j, k] : v)
+    {
+        i >> j;
+        i >> k;
+    }
+    return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v)
+{
+    for (auto &&j : v)
+        i >> j;
+    return i;
+}
+
+struct in
+{
+    template <class T>
+    operator T()
+    {
+        T t;
+        std::cin >> t;
+        return t;
+    }
+};
+
+struct Solver
+{
+    void solve()
+    {
+        i32 n = in(), k = in();
+        double ans = 0;
+        for (i32 x = 1; x <= n; x++)
+        {
+            if(x >= k)
+            {
+                ans += 1.0;
+                continue;
+            }
+            i32 num = -ceil(log2((double)k / x));
+            ans += pow(2, num);
+        }
+        ans /= n;
+        cout << ans << '\n';
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+    Solver solver;
+    solver.solve();
+}
