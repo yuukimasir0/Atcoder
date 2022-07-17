cac6c9ae5dc93f62c2457a922c4fd798e7b1a7ae
yuukimasir0 <plusion00@icloud.com>
2022-07-17 10:57:47 +0000

    [AC] abc166 abc166_d

---
+++ atcoder.jp/abc166/abc166_d/Main.cpp
@@ -0,0 +1,78 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef intmax_t ll;
+typedef int_fast64_t i64;
+typedef int_fast32_t i32;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+
+template <typename T, typename U>
+istream &operator>>(istream &i, vc<pair<T, U>> &v)
+{
+    for (auto &&[j, k] : v)
+    {
+        i >> j;
+        i >> k;
+    }
+    return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v)
+{
+    for (auto &&j : v)
+        i >> j;
+    return i;
+}
+
+struct in
+{
+    template <class T>
+    operator T()
+    {
+        T t;
+        std::cin >> t;
+        return t;
+    }
+};
+
+struct Solver
+{
+    void solve()
+    {
+        ll n = in();
+        map<ll, ll> ex;
+        for (ll x = -1000; x <= 1000; x++)
+            ex[1LL * x * x * x * x * x] = x;
+        for (auto &&[i, j] : ex)
+        {
+            if (ex.count(n + i))
+            {
+                ll a = ex[n + i];
+                cout << a << " " << j << '\n';
+                return;
+            }
+        }
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+    Solver solver;
+    solver.solve();
+}
