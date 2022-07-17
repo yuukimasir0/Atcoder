df7a07c91bc17b4d27a70e8eae94295affc110f2
yuukimasir0 <plusion00@icloud.com>
2022-07-17 08:48:16 +0000

    [AC] abc175 abc175_c

---
+++ atcoder.jp/abc175/abc175_c/Main.cpp
@@ -0,0 +1,76 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef intmax_t ll;
+typedef int_fast64_t i64;
+typedef int_fast32_t i32;
+constexpr int inf = 2e9;
+constexpr ll llinf = 9e18;
+
+template <typename T, typename U>
+istream &operator>>(istream &i, vc<pair<T, U>> &v)
+{
+    for (auto &&[j, k] : v)
+    {
+        i >> j;
+        i >> k;
+    }
+    return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v)
+{
+    for (auto &&j : v)
+        i >> j;
+    return i;
+}
+
+struct in
+{
+    template <class T>
+    operator T()
+    {
+        T t;
+        std::cin >> t;
+        return t;
+    }
+};
+
+struct Solver
+{
+    void solve()
+    {
+        ll x = in(), k = in(), d = in();
+        if (x == 0)
+        {
+            cout << (k % 2 ? d : 0) << '\n';
+            return;
+        }
+        if (x < 0)
+            x = -x;
+        ll temp = x - d * min((x / d), k);
+        k -= min((x / d), k);
+        cout << (k % 2 ? abs(temp - d) : abs(temp)) << '\n';
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+    Solver solver;
+    solver.solve();
+}
