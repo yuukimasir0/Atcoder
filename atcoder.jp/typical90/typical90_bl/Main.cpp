d01c4074f79de1fa3f738b5ec8682983469a7829
yuukimasir0 <plusion00@icloud.com>
2022-07-18 22:41:04 +0000

    [AC] typical90 typical90_bl

---
+++ atcoder.jp/typical90/typical90_bl/Main.cpp
@@ -0,0 +1,100 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef intmax_t ll;
+typedef int_fast64_t i64;
+typedef int_fast32_t i32;
+// constexpr int inf = 2e9;
+// constexpr ll llinf = 9e18;
+
+template <typename T, typename U>
+istream &operator>>(istream &i, pair<T, U> &v)
+{
+  auto &&[j, k] = v;
+  i >> j;
+  i >> k;
+  return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v)
+{
+  for (auto &&j : v)
+    i >> j;
+  return i;
+}
+
+template <typename T>
+ostream &operator<<(ostream &i, vc<T> &v)
+{
+  for (auto &&j : v)
+    cout << j;
+  return i;
+}
+
+struct in
+{
+  template <class T>
+  operator T()
+  {
+    T t;
+    std::cin >> t;
+    return t;
+  }
+};
+
+struct Solver
+{
+  void solve()
+  {
+    i32 n = in(), q = in();
+    vc<i64> a(n);
+    cin >> a;
+    vc<i64> s(n);
+    i64 ans = 0;
+    for (i32 i = 1; i < n; i++) {
+      s[i] = a[i] - a[i - 1];
+      ans += abs(s[i]);
+    }
+
+    while (q--)
+    {
+      i32 l = in(), r = in(), v = in();
+      l--;
+      r--;
+      if(l != 0)
+        {
+            ans -= abs(s[l]);
+            s[l] = v + s[l];
+            ans += abs(s[l]);
+        }
+      if(r != n - 1)
+      {
+        ans -= abs(s[r + 1]);
+        s[r + 1] = s[r + 1] - v;
+        ans += abs(s[r + 1]);
+      }
+      cout << ans << '\n';
+    }
+  }
+};
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+  Solver solver;
+  solver.solve();
+}
