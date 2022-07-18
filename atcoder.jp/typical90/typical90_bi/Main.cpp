984ab6dce254e359bd925aa6f98336d2bdb2372f
yuukimasir0 <plusion00@icloud.com>
2022-07-18 22:08:31 +0000

    [AC] typical90 typical90_bi

---
+++ atcoder.jp/typical90/typical90_bi/Main.cpp
@@ -0,0 +1,83 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef intmax_t ll;
+typedef int_fast64_t i64;
+typedef int_fast32_t i32;
+// constexpr int inf = 2e9;
+// constexpr ll llinf = 9e18;
+
+template <typename T, typename U>
+istream &operator>>(istream &i, pair<T, U> &v)
+{
+  auto &&[j, k] = v;
+  i >> j;
+  i >> k;
+  return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v)
+{
+  for (auto &&j : v)
+    i >> j;
+  return i;
+}
+
+template <typename T>
+ostream &operator<<(ostream &i, vc<T> &v)
+{
+  for (auto &&j : v)
+    cout << j;
+  return i;
+}
+
+struct in
+{
+  template <class T>
+  operator T()
+  {
+    T t;
+    std::cin >> t;
+    return t;
+  }
+};
+
+struct Solver
+{
+  void solve()
+  {
+    i32 q = in();
+    deque<i32> deq;
+    while(q--)
+    {
+      i32 t = in(), x = in();
+      if(t == 1)
+        deq.push_front(x);
+      else if(t == 2)
+        deq.push_back(x);
+      else
+        cout << deq.at(--x) << '\n';
+    }
+  }
+};
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+  Solver solver;
+  solver.solve();
+}
