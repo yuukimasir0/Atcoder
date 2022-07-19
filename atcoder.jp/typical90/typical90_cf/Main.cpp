9fec781c4e52f23e38b4ab0072a95b001a2387e2
yuukimasir0 <plusion00@icloud.com>
2022-07-19 14:35:30 +0000

    [AC] typical90 typical90_cf

---
+++ atcoder.jp/typical90/typical90_cf/Main.cpp
@@ -0,0 +1,100 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef long long ll;
+typedef long long i64;
+typedef int i32;
+// constexpr i32 inf = 2e9;
+// constexpr i64 llinf = 9e18;
+// constexpr i32 mod =
+
+template <typename T, typename U>
+istream &operator>>(istream &i, pair<T, U> &v)
+{
+  auto &&[j, k] = v;
+  i >> j;
+  i >> k;
+  return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v)
+{
+  for (auto &&j : v)
+    i >> j;
+  return i;
+}
+
+template <typename T>
+ostream &operator<<(ostream &i, vc<T> &v)
+{
+  for (auto &&j : v)
+    cout << j;
+  return i;
+}
+
+struct in
+{
+  template <class T>
+  operator T()
+  {
+    T t;
+    std::cin >> t;
+    return t;
+  }
+};
+
+struct Solver
+{
+  void solve()
+  {
+    i64 n = in();
+    string s = in();
+    i64 l = 0, r = 1;
+    i64 ans = n * (n + 1) / 2;
+    auto v = rle(s, n);
+    for(auto &&i : v)
+      ans -= i * (i + 1) / 2;
+    cout << ans << '\n';
+  }
+
+  vc<i64> rle(string &s, i64 n)
+  {
+    char temp = s[0];
+    i64 now = 1;
+    vc<i64> res;
+    for (i64 i = 1; i < n; i++)
+    {
+      if (s[i] == temp)
+        now++;
+      else
+      {
+        res.push_back(now);
+        now = 1;
+        temp = s[i];
+      }
+    }
+    res.push_back(now);
+    return res;
+  }
+};
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+  Solver solver;
+  solver.solve();
+}
