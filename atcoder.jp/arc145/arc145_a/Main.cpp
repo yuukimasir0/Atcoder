18601f61f26a1a59c7ebeec48a83c2ff4fa43a7b
yuukimasir0 <plusion00@icloud.com>
2022-07-30 12:31:26 +0000

    [AC] arc145 arc145_a

---
+++ atcoder.jp/arc145/arc145_a/Main.cpp
@@ -0,0 +1,93 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+bool chmin(T &a, T b) {
+  return a > b ? a = b, true : false;
+}
+template <typename T>
+bool chmax(T &a, T b) {
+  return a < b ? a = b, true : false;
+}
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef long long ll;
+typedef long long i64;
+typedef int i32;
+// constexpr i32 inf = 2e9;
+// constexpr i64 llinf = 9e18;
+// constexpr i32 mod =
+
+template <typename T, typename U>
+istream &operator>>(istream &i, pair<T, U> &v) {
+  auto &&[j, k] = v;
+  i >> j;
+  i >> k;
+  return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v) {
+  for (auto &&j : v) i >> j;
+  return i;
+}
+
+template <typename T>
+ostream &operator<<(ostream &i, vc<T> &v) {
+  for (auto &&j : v) cout << j << ' ';
+  cout << '\n';
+  return i;
+}
+
+struct in {
+  template <class T>
+  operator T() {
+    T t;
+    std::cin >> t;
+    return t;
+  }
+};
+
+struct Solver {
+  void solve() {
+    i32 n = in();
+    bool flag = false;
+    string s = in();
+    if(n == 1) {
+      cout << "Yes\n";
+      return;
+    }
+    if(n == 2) {
+      if(s[0] != s[1]) {
+        cout << "No\n";
+        return;
+      }
+    }
+    if(s[0] == 'B') {
+      cout << "Yes\n";
+      return;
+    }
+    if (s[n - 1] == 'A') {
+      cout << "Yes\n";
+      return;
+    }
+    cout << "No\n";
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+  Solver solver;
+  solver.solve();
+}
