05f786428dd75b9a30d4828030d153acbd6d49d3
yuukimasir0 <plusion00@icloud.com>
2022-08-03 15:03:01 +0000

    [AC] abc156 abc156_c

---
+++ atcoder.jp/abc156/abc156_c/Main.cpp
@@ -0,0 +1,88 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+template <typename T>
+bool chmin(T &a, T b) {
+  return a > b ? a = b, true : false;
+}
+template <typename T>
+bool chmax(T &a, T b) {
+  return a < b ? a = b, true : false;
+}
+template <typename T>
+bool ch(T &a, T &b) {
+  return a < b ? swap(a, b), true : false;
+}
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using pQ = priority_queue<T, vc<T>, greater<T>>;
+template <typename T>
+using Pq = priority_queue<T, vc<T>, less<T>>;
+template <typename T>
+using vv = vc<vc<T>>;
+typedef long long ll;
+typedef long long i64;
+typedef int i32;
+// constexpr i32 inf = 2e9;
+// constexpr i64 llinf = 9e18;
+// constexpr i32 mod =
+
+template <typename T, typename U>
+istream &operator>>(istream &i, pair<T, U> &v) {
+  auto &&[j, k] = v;
+  i >> j;
+  i >> k;
+  return i;
+}
+
+template <typename T>
+istream &operator>>(istream &i, vc<T> &v) {
+  for (auto &&j : v) i >> j;
+  return i;
+}
+
+template <typename T>
+ostream &operator<<(ostream &i, vc<T> &v) {
+  for (auto &&j : v) cout << j << ' ';
+  cout << '\n';
+  return i;
+}
+
+struct in {
+  template <class T>
+  operator T() {
+    T t;
+    std::cin >> t;
+    return t;
+  }
+};
+
+struct Solver {
+  void solve() {
+    i32 n = in();
+    vc<i32> x(n);
+    cin >> x;
+    sort(all(x));
+    i32 M = *max_element(all(x));
+    i32 ans = INT32_MAX;
+    for (i32 i = 1; i <= M; i++) {
+      i32 temp = 0;
+      for (const auto &j : x)
+        temp += (i - j) * (i - j);
+      chmin(ans, temp);
+    }
+    cout << ans << '\n';
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+  Solver solver;
+  solver.solve();
+}
