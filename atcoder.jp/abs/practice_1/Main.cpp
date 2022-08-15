4ac8bed40ecdc8bba5ce6128e22430fb1ed74012
yuukimasir0 <plusion00@icloud.com>
2022-08-15 19:38:29 +0000

    [AC] abs practice_1

---
+++ atcoder.jp/abs/practice_1/Main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+#define はじめ int main(){
+#define おわり }
+
+#define aは整数だよ int a;
+#define bは整数だよ int b;
+#define cは整数だよ int c;
+#define sは文字列だよ std::string s;
+
+#define aを受け取るよ std::cin >> a;
+#define bを受け取るよ std::cin >> b;
+#define cを受け取るよ std::cin >> c;
+#define sを受け取るよ std::cin >> s;
+
+#define aとbとcの和だよ std::cout << a + b + c << ' ';
+#define sだよ std::cout << s << '\n';
+
+はじめ aは整数だよ bは整数だよ cは整数だよ sは文字列だよ aを受け取るよ bを受け取るよ cを受け取るよ sを受け取るよ aとbとcの和だよ sだよ おわり
