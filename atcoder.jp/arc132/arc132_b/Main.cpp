2774c93f8a76214427342bb8861420ea12e4a1a8
yuukimasir0 <plusion00@icloud.com>
2022-08-20 11:00:45 +0000

    [AC] arc132 arc132_b

---
+++ atcoder.jp/arc132/arc132_b/Main.cpp
@@ -0,0 +1,116 @@
+#pragma region
+using namespace std;
+#include <bits/stdc++.h>
+
+#define overload4(_1, _2, _3, _4, name, ...) name
+
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define rep1(n)          for(int i = 0; i < n; i++)
+#define rep2(i, n)       for(int i = 0; i < n; i++)
+#define rep3(i, a, b)    for(int i = a; i < b; i++)
+#define rep4(i, a, b, c) for(int i = a; i < b; i += c)
+#define rep(...)         overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define rrep1(n)          for(int i = (n - 1); i >= 0; i--)
+#define rrep2(i, n)       for(int i = (n - 1); i >= 0; i--)
+#define rrep3(i, a, b)    for(int i = (b - 1); i >= (a); i--)
+#define rrep4(i, a, b, c) for(int i = (b - 1); i >= (a); i -= (c))
+#define rrep(...)         overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define LL(...)  \
+i64 __VA_ARGS__; \
+input(__VA_ARGS__)
+
+#define STR(...)    \
+string __VA_ARGS__; \
+input(__VA_ARGS__)
+
+#define VC(typename, name, size) \
+vc<typename> name(size);         \
+input(name);
+
+template<class T, class... U> void print(T &a, U &...b) {
+    cout << a;
+    (cout << ... << (cout << ' ', b));
+    cout << '\n';
+}
+
+#ifdef _MASIR0
+#define debug(...) print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+template<class... T> constexpr auto min(T... a) { return min(initializer_list<common_type_t<T...>>{a...}); }
+template<class... T> constexpr auto max(T... a) { return max(initializer_list<common_type_t<T...>>{a...}); }
+template<class T> auto min(const T &a) { return *min_element(all(a)); }
+template<class T> auto max(const T &a) { return *max_element(all(a)); }
+
+template<class... T> void input(T &...a) { (cin >> ... >> a); }
+template<typename T> bool chmin(T &a, T b) { return a > b ? a = b, true : false; }
+template<typename T> bool chmax(T &a, T b) { return a < b ? a = b, true : false; }
+
+template<typename T> using vc = vector<T>;
+template<typename T> using pQ = priority_queue<T, vc<T>, greater<T>>;
+template<typename T> using Pq = priority_queue<T, vc<T>, less<T>>;
+template<typename T> using vv = vc<vc<T>>;
+
+typedef long long ll;
+typedef long long i64;
+typedef int i32;
+typedef long double ld;
+// constexpr i32 mod = 1e9 + 7; // 998244353;
+template<typename T, typename U> istream &operator>>(istream &i, pair<T, U> &v) {
+    auto &&[j, k] = v;
+    i >> j;
+    i >> k;
+    return i;
+}
+
+template<typename T> istream &operator>>(istream &i, vc<T> &v) {
+    for(auto &&j : v) i >> j;
+    return i;
+}
+
+template<typename T> ostream &operator<<(ostream &i, vc<T> &v) {
+    for(auto &&j : v) cout << j << ' ';
+    cout << '\n';
+    return i;
+}
+#pragma endregion
+
+struct Solver {
+    void solve() {
+        LL(n);
+        VC(i64, p, n);
+        i64 ans = calc(p);
+        reverse(all(p));
+        chmin(ans, calc(p) + 1);
+        print(ans);
+    }
+
+    i64 calc(vc<i64> &p) {
+        i64 res = 0;
+        i64 n = p.size();
+        rep(n) {
+            if(p[i] == 1) break;
+            res++;
+        }
+        if(res != 0) {
+            if(p[res - 1] == n) return res;
+            else return res + 2;
+        }
+        if(p[1] == 2) return res;
+        return 2;
+    }
+};
+
+signed main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+    Solver solver;
+    solver.solve();
+}
